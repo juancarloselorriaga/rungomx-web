4caf9404e21360b32c4f127e8e37eb96358dcd50
Juan Carlos Elorriaga <juancarloselorriaga@gmail.com>
2025-11-23 17:14:12 +0100

    feat: update TypeScript definitions for next-intl translations and add AppConfig interface

---
+++ i18n/types.ts
@@ -0,0 +1,11 @@
+import { AppLocale } from '@/i18n/routing';
+import type messages from '@/messages/en.json';
+
+export type Messages = typeof messages;
+
+declare module 'next-intl' {
+  interface AppConfig {
+    Locale: AppLocale;
+    Messages: Messages;
+  }
+}
